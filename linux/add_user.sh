f5c8130ac86e0bf8416268b2af6a7d017b3ee57e
Nate Crisler <50711057+natecrisler@users.noreply.github.com>
2020-12-04 18:27:10 -0500

    removing ssh-keygen
    should make user less prone to usage as a jump host

--- linux/add_user.sh
+++ linux/add_user.sh
@@ -4,24 +4,22 @@
 # ------------------------------------------------------------------
 # Am i Root user?
 if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -p "Enter Publickey : " pub_key
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		useradd -m "$username"
-		[ $? -eq 0 ] && echo "User has been added!" || echo "Failed to add a user!"
-		echo "Generating Keys"
-		mkdir /home/$username/.ssh/ ; chown -R $username:$username /home/$username/.ssh/
-		ssh-keygen -t rsa -N '' -f "/home/$username/.ssh/id_rsa"
-		cat /home/$username/.ssh/id_rsa.pub >> /home/$username/.ssh/authorized_keys ; chmod 700 /home/$username/.ssh ; chmod 600 /home/$username/.ssh/authorized_keys
-		echo "$pub_key" >> /home/$username/.ssh/authorized_keys
-		chown -R $username:$username /home/$username/.ssh/
-		ssh-add
-	fi
+        read -p "Enter username : " username
+        read -p "Enter Publickey : " pub_key
+        egrep "^$username" /etc/passwd >/dev/null
+        if [ $? -eq 0 ]; then
+                echo "$username exists!"
+                exit 1
+        else
+                useradd -m "$username"
+                [ $? -eq 0 ] && echo "User has been added!" || echo "Failed to add a user!"
+                echo "Adding Public Key"
+                mkdir /home/$username/.ssh/ ; chown -R $username:$username /home/$username/.ssh/
+                echo "$pub_key" >> /home/$username/.ssh/authorized_keys ; chmod 600 /home/$username/.ssh/authorized_keys
+                chown -R $username:$username /home/$username/.ssh/
+                usermod -a -G wheel $username
+        fi
 else
-	echo "Must be ROOT to add user"
-	exit 2
+        echo "Must be ROOT to add user"
+        exit 2
 fi
